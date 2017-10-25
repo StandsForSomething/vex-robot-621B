bc9b4b10e3d6afd2e37fb447db5f811c21006897
Josiah Atwell <jlatwell@protonmail.com>
2017-10-25 17:28:39 -0400

    allow arm to go down with more force

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -71,7 +71,7 @@ void operatorControl() {
 			}
 
 			else if(buttonGetState(JOY1_5D)) {
-				fbcSetGoal(&arm1FBC, (int)getSensor(arm1Pot) - 25);
+				fbcSetGoal(&arm1FBC, (int)getSensor(arm1Pot) - 150);
 			}
 
 			if(arm1FBC.goal < ARM_1_BOTTOM) {
